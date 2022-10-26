9ebbbd5f9d5d3fd3ad28427ef0d1d8369cf00510
GC-bingbingdeng <bingbing.deng@grandchallenge.co.jp>
2022-10-26 14:14:59 +0900

    first commit

---
+++ wp-content/themes/boldlab/nga.js
@@ -0,0 +1,148 @@
+// 
+// 
+// // 个人简介
+window.onpageshow=function(){
+	
+	var lawer = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-34b6d96.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > section > div > div.elementor-column.elementor-col-33.elementor-inner-column.elementor-element.elementor-element-50a561b > div > div.elementor-element.elementor-element-0d3bda5.elementor-widget.elementor-widget-wp-widget-custom_html > div > div > div > table > tbody > tr:nth-child(6) > th")
+if(lawer != undefined){
+	// about lawer width
+	lawer.setAttribute('style','width:200px')
+	
+}
+
+	
+		var wqa = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(1) > div > div.qodef-e-content");
+		if(wqa!= undefined){
+
+			var wxp = document.createElement('p');
+			wxp.id = 'wq';
+			wxp.style.cssText = 'width:270px;';
+			wxp.innerText = `慶應義塾大学商学部
+Recruit HD (事業開発・新規事業統括)
+JCCD.com (CEO)
+AiBank.jp (CEO)
+			`;
+			wqa.appendChild(wxp)
+		}
+
+
+
+
+		var wrt = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(7) > div > div.qodef-e-content");
+		if(wrt!= undefined){
+
+			var wrtp = document.createElement('p');
+			wrtp.id = 'wq';
+			wrtp.style.cssText = 'width:270px;';
+			wrtp.innerText = `早稲田大学大学院商学研究科`;
+			wrt.appendChild(wrtp)
+		}
+
+
+
+		var zh = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(2) > div > div.qodef-e-content");
+		if(zh!= undefined){
+
+			var zhp = document.createElement('p');
+			zhp.id = 'wq';
+			zhp.style.cssText = 'width:270px;';
+			zhp.innerText = `早稲田大学大学院商学研究科
+Sharp (BD)  
+Alibaba Japan (BD) 
+
+
+
+			`;
+			zh.appendChild(zhp)
+
+		}
+
+	var yjj = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(6) > div > div.qodef-e-content");
+		if(yjj!= undefined){
+
+			var yjjp = document.createElement('p');
+			yjjp.id = 'wq';
+			yjjp.style.cssText = 'width:270px;';
+			yjjp.innerText = `慶應義塾大学商学部
+電通（Global Media）
+Tokyo Olympic Organizing Committee
+			`;
+			yjj.appendChild(yjjp)
+			
+		}
+
+	var nk = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(3) > div > div.qodef-e-content");
+		if(nk!= undefined){
+
+			var nkp = document.createElement('p');
+			nkp.id = 'wq';
+			nkp.style.cssText = 'width:270px;';
+			nkp.innerText = `大連交通大学Software工学
+IBM China
+Yahoo Japan
+Konami Holdings Corporation
+CyberAgent, Inc.
+En Japan Inc. President's Office
+Globis MBA
+
+
+			`;
+			nk.appendChild(nkp)
+		}
+	
+	var zl = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(8) > div > div.qodef-e-content");
+		if(zl!= undefined){
+
+			var zlp = document.createElement('p');
+			zlp.id = 'wq';
+			zlp.style.cssText = 'width:270px;';
+			zlp.innerText = `明治大学大学院
+			`;
+			zl.appendChild(zlp)
+		}
+	
+		var xiaoxue = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(4) > div > div.qodef-e-content");
+		if(zh!= undefined){
+
+			var xiaoxuep = document.createElement('p');
+			xiaoxuep.id = 'wq';
+			xiaoxuep.style.cssText = 'width:270px;';
+			xiaoxuep.innerText = `上海交通大学
+Fast Retailing (CS)
+Paypal (CS)
+Apple Japan (HR)
+`;
+			xiaoxue.appendChild(xiaoxuep)
+		}
+	
+		
+		var lyx = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(5) > div > div.qodef-e-content");
+		if(zh!= undefined){
+
+			var lyxp = document.createElement('p');
+			lyxp.id = 'wq';
+			lyxp.style.cssText = 'width:270px;';
+			lyxp.innerText = `上海外国語大学日本文化経済学院
+Vector Inc.（PR Consultant）
+APCO Worldwide（Senior Consultant）
+`;
+			lyx.appendChild(lyxp)
+		}
+	
+			var ibun = document.querySelector("#qodef-page-content > div > div > div > div > section.elementor-section.elementor-top-section.elementor-element.elementor-element-b311bbd.elementor-section-boxed.elementor-section-height-default.elementor-section-height-default.qodef-elementor-content-no.qodef-content-alignment-no > div > div > div > div > div > div > div > div:nth-child(10) > div > div.qodef-e-content");
+		if(zh!= undefined){
+
+			var ibunp = document.createElement('p');
+			ibunp.id = 'wq';
+			ibunp.style.cssText = 'width:270px;';
+			ibunp.innerText = `早稲田大学国際教養学部
+World Road. Inc（Founder/CEO/Chief Branding Officer）
+HI合同会社 (CEO)
+One Young World (Japan Ambassador)
+朝日新聞（Commentator）
+`;
+			ibun.appendChild(ibunp)
+		}
+}
+
+
